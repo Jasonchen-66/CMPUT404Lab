fe77693df61ea519a74f028de66bad4e3b6e16d5
Jasonchen-66 <jianbang@ualberta.ca>
2022-09-14 18:15:59 -0600

    update version.py

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 import requests
 print(requests.__version__)
 
-response = requests.get("https://www.google.com")
+response = requests.get("https://raw.githubusercontent.com/Jasonchen-66/CMPUT455/master/version.py")
 print(response.text)
